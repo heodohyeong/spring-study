886de491ec8880b38e184f00cb34246040de517e
heodohyeong <jackt5000@naver.com>
2022-07-08 23:27:36 +0900

    logger

--- springWeb/src/main/java/my/first/spring/batch/SampleBatchController.java
+++ springWeb/src/main/java/my/first/spring/batch/SampleBatchController.java
@@ -1,12 +1,16 @@
 package my.first.spring.batch;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component("sampleTest")
 public class SampleBatchController {
 	
-	
+	private final Logger log = LoggerFactory.getLogger(SampleBatchController.class);
 	public void sampleBatch() {
+		log.debug("log test");
 		System.out.println("배치시작");
 		System.out.println("배치종료");
 	}
